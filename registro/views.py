936ca81f7744e376aa93e77a1e1605b16c315818
Pablo171232 <yaguaracutopablo1712@gmail.com>
2024-10-12 12:06:04 -0400

    empezando registro

--- registro/views.py
+++ registro/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
+from django.views.generic import TemplateView
 # Create your views here.
 class SignUpView(CreateView):
     
     form_class = UserCreationForm
     suces_url = reverse_lazy("login")
     template_name = "registration/signup"
-    
+
     class LogoutView(TemplateView):
         template_name = "logout.html"
